f5dfe7031e6b27651d6602c7032e1a523eeea3e4
Philip Loa <boyet007@gmail.com>
2018-08-25 10:40:23 +0700

    vue loader default setting

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,8 +15,9 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    component: '<example-component></example-component>'
 });
